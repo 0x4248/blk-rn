3a5ee6aab0cb485304442923270ba195e3b2871c
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:57:02 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/blkrn.cpp
+++ src/blkrn.cpp
@@ -1,8 +1,8 @@
 /* BLK-RN
  * A lightweight bulk rename tool for Linux
- * Github: https://www.github.com/lewisevans2007/blk-rn
+ * Github: https://www.github.com/0x4248/blk-rn
  * Licence: GNU General Public License v3.0
- * By: Lewis Evans
+ * By: 0x4248
  */
 
 #include <filesystem>
